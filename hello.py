7417073215e6a6f4ba10ce175269529a0b995a52
imdungE <hulipatbingsumukbang@gmal.com>
2025-03-12 14:01:19 +0900

    2

--- hello.py
+++ hello.py
@@ -20,3 +20,12 @@ print()
 print("--- 확인 전용선 ---")
 
 print('"안녕하세요"[0:2]:', "안녕하세요"[0:2])
+
+
+input_a = float(input("첫 번째 숫자> "))
+input_b = float(input("두 번째 숫자> "))
+
+print("덧셈 결과:", input_a + input_b)
+print("뺄셈 결과:", input_a - input_b)
+print("곱셈 결과:", input_a * input_b)
+print("나눗셈 결과:", input_a / input_b)

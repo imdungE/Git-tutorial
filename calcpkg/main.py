586fa457ba496e119886e1bb8ac8b2b40c7300ca
imdungE <hulipatbingsumukbang@gmal.com>
2025-04-16 12:55:47 +0900

    rne

---
+++ calcpkg/main.py
@@ -0,0 +1,8 @@
+import calcpkg.operation
+import calcpkg.geomatry
+
+print(calcpkg.operation.add(10, 20))
+print(calcpkg.operation.mul(10, 20))
+
+print(calcpkg.geomatry.triangle_area(30,40))
+print(calcpkg.geomatry.rectangle_area(30,40))

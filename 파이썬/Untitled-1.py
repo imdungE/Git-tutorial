6c36f6490af1a377e23b769e92cc7dae3ce54cfa
imdungE <hulipatbingsumukbang@gmal.com>
2025-04-19 21:04:35 +0900

    ljnlds

--- 파이썬/Untitled-1.py
+++ 파이썬/Untitled-1.py
@@ -1,24 +1,3 @@
-def para_func (*para) : 
-    result = 0
-    for num in para : 
-        result = result + num
-    
-    return result
-
-def dic_func (**para) :
-    for k in para.keys() :
-        print("%s --> %d명입니다." %(k,para[k]))
-    print(para)
-
-dic_func(트와이스 = 9, 소녀시대 = 7, 걸스데이 = 4, 블랙핑크 = 4)
-
-hap = 0
-
-hap = para_func(10,20)
-print("매개변수가 2개인 함수를 호출한 결과 ==> %d" %hap)
-hap = para_func(10,20,30)
-print("매개변수가 3개인 함수를 호출한 결과 ==> %d" %hap)
-
 import random
 
 def getNumber() :
@@ -29,15 +8,19 @@ num = 0
 
 print("** 로또 추첨을 시작합니다. **")
 
+
+
+
 while True :
     num = getNumber()
     if lotto.count(num) == 0 :
         lotto.append(num)
-        if len(lotto) >= 6 : 
-            break
+
+    if len(lotto) >= 6 : 
+        break
 
 print("추첨된 로또 번호 ==> ", end = '')
 lotto.sort()
-for i in range(0,6) :
+for i in range(6) :
     print("%d " %lotto[i], end = '')
 
--- 파이썬/실습.py
+++ 파이썬/실습.py
@@ -12,3 +12,11 @@ def ㅆ씨발2(**니엄마) :
     for k in 니엄마.keys():
         print(k, ":", 니엄마[k])
 ㅆ씨발2(이름="김철수", 나이=20, 성별="남자")
+
+def genfunc(num) :
+    for i in range(0, num) :
+        yield i
+        print("제너레이터 진행 중")
+
+for data in genfunc(5) :
+    print(data)

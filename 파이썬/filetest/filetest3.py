8b6269ccec1603ba91fc961132938ecdd7487c59
imdungE <hulipatbingsumukbang@gmal.com>
2025-05-14 13:56:02 +0900

    fldknfdl

---
+++ 파이썬/filetest/filetest3.py
@@ -0,0 +1,13 @@
+inFp, outFp = None, None
+inStr = ""
+
+inFp = open("C:\\Users\\titic\\OneDrive\\바탕 화면\\Github\\Git-tutorial\\파이썬\\문찬주.txt", "r", encoding = "utf-8")
+outFp = open("C:\\Users\\titic\\OneDrive\\바탕 화면\\Github\\Git-tutorial\\파이썬\\문찬주2.txt", "w", encoding = "utf-8")
+
+inList = inFp.readlines()
+for inStr in inList :
+    outFp.writelines(inStr)
+
+inFp.close()
+outFp.close()
+print("----파일 복사 완료----")
---
+++ 파이썬/이진파일입출력.py
@@ -0,0 +1,45 @@
+from tkinter import *
+
+def loadImage (fname) :
+    global inImage, XSIZE, YSIZE
+    fp = open(fname, 'rb')
+
+    for i in range(0, XSIZE) :
+        tmpList = []
+        for k in range(0, YSIZE) :
+            data = int(ord(fp.read(1)))
+            tmpList.append(data)
+        inImage.append(tmpList)
+
+    fp.close()
+
+def displayImage(image) :
+    global XSIZE, YSIZE
+    rgbString = ""
+    for i in range(0, XSIZE) :
+        for k in range(0, YSIZE) :
+            tmpString = ""
+            for k in range(0, YSIZE) :
+                data = image[i][k]
+                tmpString += "#%02x%02x%02x " % (data,data,data)
+        rgbString += "{" + tmpString + "} "
+    paper.put(rgbString)
+    
+window = None
+canvas = None
+XSIZE , YSIZE = 256, 256
+inImage = []
+
+window = Tk()
+window.title("흑백사진보기")
+canvas= Canvas(window, width=XSIZE, height=YSIZE)
+paper = PhotoImage(width=XSIZE, height=YSIZE)
+canvas.create_image((XSIZE/2, YSIZE/2), image=paper, state="normal")
+
+filename = "C:\\Users\\titic\\OneDrive\\바탕 화면\\파이썬\\Lenna.raw"
+loadImage(filename)
+
+displayImage(inImage)
+
+canvas.pack()
+window.mainloop()

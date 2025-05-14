bc84793f39018e223e104263999d3ef4b2001ef8
imdungE <hulipatbingsumukbang@gmal.com>
2025-05-14 12:38:34 +0900

    bb

--- 파이썬/filetest/filetest.py
+++ 파이썬/filetest/filetest.py
@@ -1,9 +1,18 @@
+import os
+
 inFp = None
-inList = ""
+fName, inList, inStr = "", [], ""
 
-inFp = open("C:\\Users\\titic\\OneDrive\\바탕 화면\\Github\\Git-tutorial\\파이썬\\문찬주.txt", "r")
+fName = input("파일명을 입력하세요 : ")
 
-inList = inFp.readlines()
-print(inList)
+if os.path.exists(fName) :
+    inFp = open(fName, "r", encoding = "utf-8")
 
-inFp.close()
+    inList = inFp.readlines()
+    for inStr in inList :
+        print(inStr, end = "")
+
+    inFp.close()
+
+else :
+    print("파일이 존재하지 않습니다.")

440043970762d56a904a6fd2ab0034ba78b45e16
imdungE <hulipatbingsumukbang@gmal.com>
2025-06-06 17:42:20 +0900

    ,ㅎ;ㄹㄹ

--- 파이썬/파이썬 수업/9주차/10-10.py
+++ 파이썬/파이썬 수업/9주차/10-10.py
@@ -7,6 +7,6 @@ for i in range(0, 3) :
     btnList[i] = Button(window, text = "버튼" + str(i + 1))
 
 for btn in btnList :
-    btn.pack(side = RIGHT)
+    btn.pack(side = LEFT)
     
 window.mainloop()

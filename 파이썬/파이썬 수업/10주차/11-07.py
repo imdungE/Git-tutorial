f22c8641bfbd099f52bd29653918104aada75994
imdungE <hulipatbingsumukbang@gmal.com>
2025-06-08 00:10:49 +0900

    Ekqhs

--- 파이썬/파이썬 수업/10주차/11-07.py
+++ 파이썬/파이썬 수업/10주차/11-07.py
@@ -1,4 +1,13 @@
 outFp = None
 outStr = ""
 
-outFp = open("C:\\Users\\titic\\OneDrive\\바탕 화면\\Github\\Git-tutorial\\파이썬\\문찬주.txt", "w")
+outFp = open("C:\\Users\\titic\\OneDrive\\바탕 화면\\Github\\Git-tutorial\\파이썬\\문찬주.txt", "w", encoding = "utf-8")
+while True : 
+    outStr = input("내용 입력 : ")
+    if outStr != "" : 
+        outFp.writelines(outStr + "\n")
+    else : 
+        break
+
+outFp.close()
+print("---정상적으로 파일에 씀---")
